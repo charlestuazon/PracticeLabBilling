b9c8d00fa01a71ab9dac6f30d901cb412823f8dc
unknown <charleschristian.tuazon.iics@ust.edu.ph>
2020-02-14 23:16:15 +0800

    second commit

--- gallery.js
+++ gallery.js
@@ -1,4 +1,4 @@
-
+/*Addthe JavaScript here for the function billingFunction().  It is responsible for setting and clearing the fields in Billing Information */
 
 function billingFunction() {
     if (document.getElementById("same").checked) {
